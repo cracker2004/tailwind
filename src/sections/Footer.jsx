5608c71d0e7c4b9818ad2a631f3b8234ad6885c5
cracker2004 <rohit.bansal2404@gmail.com>
2024-01-06 02:39:12 +0530

    Format Everthing

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -15,20 +15,32 @@ const Footer = () => {
             Your perfect Size in Store. Get Rewards
           </p>
           <div className="flex items-center gap-5 mt-8">
-              {socialMedia.map((icon) => (
-                <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
-                  <img src={icon.src} alt={icon.alt} width={24} height={24} />
-                </div>
-              ))}
+            {socialMedia.map((icon) => (
+              <div
+                key={icon.src}
+                className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+              >
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
           </div>
         </div>
         <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
           {footerLinks.map((section) => (
             <div key={section.id}>
-              <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">{section.title}</h4>
+              <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">
+                {section.title}
+              </h4>
               <ul>
-                {section.links.map(link => (
-                  <li><a href="" className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer" key={link.name}>{link.name}</a></li>
+                {section.links.map((link) => (
+                  <li key={link.name}>
+                    <a
+                      href=""
+                      className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer"
+                    >
+                      {link.name}
+                    </a>
+                  </li>
                 ))}
               </ul>
             </div>
@@ -37,7 +49,13 @@ const Footer = () => {
       </div>
       <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
         <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
-          <img src={copyrightSign} alt="copy right sign" width={20} height={20} className="rounded-full m-0" />
+          <img
+            src={copyrightSign}
+            alt="copy right sign"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
           <p>Copyright. All rights reserved.</p>
         </div>
         <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
